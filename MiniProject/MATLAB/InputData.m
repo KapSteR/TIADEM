44137e9fcb35e31fa706cec23df37b9c7336c7d6
KapSteR <hr.kasper.nielsen@gmail.com>
2014-12-03 13:21:28 +0100

    Add calc of index values of input of data

--- MiniProject/MATLAB/InputData.m
+++ MiniProject/MATLAB/InputData.m
@@ -10,21 +10,31 @@ lon = ncread(dataPath,'lon');
 latDim =  size(lat,1);
 lonDim =  size(lon,1);
 
-northMax = ncreadatt(dataPath,'/','northernmost_latitude')
-southMax = ncreadatt(dataPath,'/','southernmost_latitude')
-westMax = ncreadatt(dataPath,'/','westernmost_longtitude')
-eastMax = ncreadatt(dataPath,'/','easternmost_longtitude')
+northMax = ncreadatt(dataPath,'/','northernmost_latitude');
+southMax = ncreadatt(dataPath,'/','southernmost_latitude');
+westMax = ncreadatt(dataPath,'/','westernmost_longitude');
+eastMax = ncreadatt(dataPath,'/','easternmost_longitude');
 
 
 % Index steps per degree
-latStep = (northMax-southMax)/latDim;
-lonStep = (eastMax-westMax)/lonDim;
+latStep = latDim/(northMax-southMax);
+lonStep = lonDim/(eastMax-westMax);
 
 northBound = 60;
 southBound = 40;
 
-westbound = 0;
-eastbound = 20;
+westBound = 0;
+eastBound = 20;
 
-northIndex = 
+% Find index plus zero offset
+northIndex = northBound*latStep + (16000/2);
+southIndex = -southBound*latStep +(16000/2);
+eastIndex  = eastBound*lonStep + (36000/2);
+westIndex  = -westBound*lonStep + (36000/2);
+
+
+
+
+
+
 
