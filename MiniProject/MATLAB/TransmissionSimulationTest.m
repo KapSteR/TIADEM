38e1deefe041621f8b58ff6a4173a5d48d60e794
KapSteR <hr.kasper.nielsen@gmail.com>
2014-12-20 00:28:15 +0100

    Start Simulation Implementation

--- MiniProject/MATLAB/TransmissionSimulationTest.m
+++ MiniProject/MATLAB/TransmissionSimulationTest.m
@@ -1,23 +1,45 @@
-RACS simulation
+% RACS simulation
+clear; clc,
 
-	- Initialization
-		- Generate random number for all data points (nodes)
-		- Choose for sensing based on p (Find(x>p))
-	
-	- Transmission:
-		- Generate random number for all selected nodes
-		- Multiply with frame time (mind the end of frame)
-		- For each selected node:
-			- search for for other nodes inside +- one transmission time
-			- If collision is detected remove index from list of received data points
+load('DATA\SST_data_subset')
 
-	- Output:
-		- List of received data points
-		- Percentage of packet reception
+ [I,J] = size(sstDataC);
+
+ N = I*J;
+
+%% Set required parameters
+
+% Probability of sensing
+p = 0.90;
+
+% Packet time
+Tp = 0.200;	% seconds
+
+% Frame time 3 hours
+T = 3600*3; % seconds
+
+
+%% 	- Initialization
+% 		- Generate random number for all data points (nodes)
+% 		- Choose for sensing based on p (Find(x>p))
+
+select = rand(N,1);
+select = find(select >= p);
 	
-	- Next:
-		- Make into a function or generic callable script
-		- Reconstruct
-		- Estimate error
-		- 
+%% 	- Transmission:
+% 		- Generate random number for all selected nodes
+% 		- Multiply with frame time (mind the end of frame)
+% 		- For each selected node:
+% 			- search for other nodes inside +- one transmission time
+% 			- If collision is detected remove index from list of received data points
+
+% 	- Output:
+% 		- List of received data points
+% 		- Percentage of packet reception
+	
+% 	- Next:
+% 		- Make into a function or generic callable script
+% 		- Reconstruct
+% 		- Estimate error
+% 		- 
 
